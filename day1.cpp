05a1345d5ac0157e90fa7e6d8a7f399ab3df65aa
AxelSiwmark1 <siwmark@net.chalmers.se>
2022-12-02 17:46:28 +0100

    day1

---
+++ day1.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+
+int main () {
+  string line;
+  ifstream myfile ("example.txt");
+  int sum = 0;
+  int max = 0;
+  if (myfile.is_open())
+  {
+    while ( getline (myfile,line) )
+    {
+        if(line != ""){
+            sum = sum + stoi(line);
+        }
+        else{
+            cout << sum << endl;
+            if(sum > max){
+              max = sum;
+            } 
+            sum = 0;
+        }
+    }
+    cout << max;
+    myfile.close();
+    cout << sum;
+  }
+
+  else cout << "Unable to open file"; 
+
+  return 0;
+}
